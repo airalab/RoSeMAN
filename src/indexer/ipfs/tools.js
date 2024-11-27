c201262ce80bad70db02910bb8a9d509a80b9576
vol4tim <vol4tim@gmail.com>
2024-11-27 14:22:36 +0400

    upd ipfs gateway

--- src/indexer/ipfs/tools.js
+++ src/indexer/ipfs/tools.js
@@ -1,31 +1,59 @@
 import axios from "axios";
+import config from "../../config";
 import logger from "../../utils/logger";
 
-const listGateways = {
-  "https://ipfs.io": 0,
-  "https://gateway.ipfs.io": 0,
-  "https://cloudflare-ipfs.com": 0,
-  "https://cf-ipfs.com": 0,
-  "https://ipfs.eth.aragon.network": 0,
-  "https://ipfs-gateway.multi-agent.io": 0,
-};
+const listGateways = (config.listOfGateways || ["https://ipfs.io"]).reduce(
+  (gateways, curr) => {
+    if (typeof curr === "string") {
+      gateways[curr] = {
+        url: `${curr}/ipfs/`,
+        method: "get",
+        options: {},
+      };
+    } else if (curr.url) {
+      gateways[curr.url] = curr;
+    }
+    return gateways;
+  },
+  {}
+);
+const counterGateways = Object.entries(listGateways).reduce(
+  (result, curr) => ((result[curr[0]] = 0), result),
+  {}
+);
 
 async function catOfGateway(hash, options) {
-  logger.debug(JSON.stringify(listGateways, null, "\t"));
-  const keys = Object.keys(
-    Object.entries(listGateways)
+  logger.debug(JSON.stringify(counterGateways, null, "\t"));
+  const sortedGateways = Object.keys(
+    Object.entries(counterGateways)
       .sort(([, a], [, b]) => b - a)
       .reduce((r, [k, v]) => ({ ...r, [k]: v }), {})
   );
-  for (const item of keys) {
+  for (const item of sortedGateways) {
     try {
-      const res = await axios.get(`${item}/ipfs/${hash}`, options);
-      listGateways[item] += 1;
+      const gateway = listGateways[item];
+      let res;
+      if (gateway.method === "post") {
+        res = await axios.post(
+          `${item}${hash}`,
+          {},
+          {
+            ...options,
+            ...gateway.options,
+          }
+        );
+      } else {
+        res = await axios.get(`${item}${hash}`, {
+          ...options,
+          ...gateway.options,
+        });
+      }
+      counterGateways[item] += 1;
       if (res.data) {
         return JSON.stringify(res.data);
       }
     } catch (error) {
-      logger.warn(`${item}/ipfs/${hash} ${error.message}`);
+      logger.warn(`${item}${hash} ${error.message}`);
     }
   }
   const e = new Error(`not found ${hash}`);
