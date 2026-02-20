32a06efcb4c9c23c8b998850a0032ba804fbe067
vol4tim <vol4tim@gmail.com>
2026-02-20 10:27:24 +0400

    switch to kusama parachain

--- src/indexer/index.js
+++ src/indexer/index.js
@@ -2,7 +2,8 @@ import config from "../config";
 import LastBlock from "../models/lastBlock";
 import { rosemanBlockRead } from "../utils/prometheus";
 import chain, { BLOCK } from "./chain";
-import { dtwin, rwsOwner, sensors } from "./chain/handlers";
+import { rwsOwner, sensors } from "./chain/handlers";
+// import { dtwin, rwsOwner, sensors } from "./chain/handlers";
 import { parser as parserDTwins } from "./ipfs/dtwin";
 import { parser as parserSensors } from "./ipfs/sensors";
 
@@ -24,16 +25,21 @@ export default function (cb) {
       start = BLOCK.DB;
     }
 
-    // chain(
-    //   config.CHAIN_API_KUSAMA,
-    //   start,
-    //   { extrinsic: ["datalog"], event: ["datalog/NewRecord"] },
-    //   { rws: [rwsOwner, sensors], datalog: [sensors] },
-    //   async (block) => {
-    //     await LastBlock.updateOne({}, { block: block }).exec();
-    //     rosemanBlockRead.set({ chain: "robonomics" }, block);
-    //   }
-    // );
+    // 9091610 - 9650000
+    chain(
+      config.CHAIN_API_KUSAMA,
+      CHAIN_NAME.KUSAMA,
+      start,
+      { extrinsic: ["datalog"], event: ["datalog/NewRecord"] },
+      { rws: [rwsOwner, sensors], datalog: [sensors] },
+      async (block) => {
+        await LastBlock.updateOne(
+          { chain: CHAIN_NAME.KUSAMA },
+          { block: block }
+        ).exec();
+        rosemanBlockRead.set({ chain: "robonomics" }, block);
+      }
+    );
 
     // chain(
     //   config.CHAIN_API_POLKADOT,
@@ -46,27 +52,27 @@ export default function (cb) {
     //   { rws: [rwsOwner, dtwin], "digitalTwin/setSource": [dtwin] }
     // );
 
-    chain(
-      config.CHAIN_API_POLKADOT,
-      CHAIN_NAME.POLKADOT,
-      start,
-      {
-        extrinsic: ["datalog", "rws", "digitalTwin/setSource"],
-        event: ["datalog/NewRecord", "digitalTwin/TopicChanged"],
-      },
-      {
-        rws: [rwsOwner, sensors, dtwin],
-        datalog: [sensors],
-        "digitalTwin/setSource": [dtwin],
-      },
-      async (block) => {
-        await LastBlock.updateOne(
-          { chain: CHAIN_NAME.POLKADOT },
-          { block: block }
-        ).exec();
-        rosemanBlockRead.set({ chain: "robonomics" }, block);
-      }
-    );
+    // chain(
+    //   config.CHAIN_API_POLKADOT,
+    //   CHAIN_NAME.POLKADOT,
+    //   start,
+    //   {
+    //     extrinsic: ["datalog", "rws", "digitalTwin/setSource"],
+    //     event: ["datalog/NewRecord", "digitalTwin/TopicChanged"],
+    //   },
+    //   {
+    //     rws: [rwsOwner, sensors, dtwin],
+    //     datalog: [sensors],
+    //     "digitalTwin/setSource": [dtwin],
+    //   },
+    //   async (block) => {
+    //     await LastBlock.updateOne(
+    //       { chain: CHAIN_NAME.POLKADOT },
+    //       { block: block }
+    //     ).exec();
+    //     rosemanBlockRead.set({ chain: "robonomics" }, block);
+    //   }
+    // );
   }
   if (config.INDEX_IPFS) {
     parserSensors(cb);
